18ab9f0b2c727c52054f2d8791be1f078da3bf2b
TerryDennisonJr <77395950+TerryDennisonJr@users.noreply.github.com>
2023-03-07 08:03:39 -0700

    Update app_setup.sh

--- app_setup.sh
+++ app_setup.sh
@@ -5,7 +5,16 @@ sudo apt install docker.io -y
 sudo apt-get install mariadb-client -y
 sudo apt install docker-compose -y
 
+cd Desktop
 mkdir weather_docker && cd weather_docker
+curl -o pi_weather_docker.py https://raw.githubusercontent.com/TerryDennisonJr/weather_pi_docker/main/pi_weather_docker.py
+curl -o Dockerfile https://raw.githubusercontent.com/TerryDennisonJr/weather_pi_docker/main/Dockerfile
+curl -o docker-compose.yaml https://raw.githubusercontent.com/TerryDennisonJr/weather_pi_docker/main/docker-compose.yaml
+
+cd ..
+
+sudo docker-compose up
+#sudo docker-compose run app
 
 
 
