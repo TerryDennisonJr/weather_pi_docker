6cb2d8cceade011016f674e6525fe8211774aaee
TerryDennisonJr <77395950+TerryDennisonJr@users.noreply.github.com>
2023-03-07 08:07:22 -0700

    Update app_setup.sh

--- app_setup.sh
+++ app_setup.sh
@@ -11,6 +11,7 @@ curl -o pi_weather_docker.py https://raw.githubusercontent.com/TerryDennisonJr/w
 curl -o Dockerfile https://raw.githubusercontent.com/TerryDennisonJr/weather_pi_docker/main/Dockerfile
 curl -o docker-compose.yaml https://raw.githubusercontent.com/TerryDennisonJr/weather_pi_docker/main/docker-compose.yaml
 
+# pwd needs to be in the path where .yaml file is located
 sudo docker-compose up
 #sudo docker-compose run app
 
